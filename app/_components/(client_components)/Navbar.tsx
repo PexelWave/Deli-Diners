7e79c179cca7bc3aaef5168e1f87530815494d59
Perfect Nkois <perfect.coding98@gmail.com>
2023-09-25 23:35:52 +0200

    Refactored Navbar.tsx and added tests for navbar

--- app/_components/(client_components)/Navbar.tsx
+++ app/_components/(client_components)/Navbar.tsx
@@ -38,12 +38,54 @@ const navLinks: NavLink[] = [
   },
 ];
 
+/**
+ * Represents a navigation bar component with a logo, menu links, and a theme switcher.
+ * @param props - The component props (currently empty).
+ * @returns The rendered navigation bar component.
+ */
 const Navbar = (props: Props) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  /**
+   * Toggles the value of `isMenuOpen` when called.
+   */
   const handleClick = () => {
     setIsMenuOpen((prev) => !prev);
   };
+
+  const renderMobileSidebar = () => {
+    if (!isMenuOpen) return null;
+
+    return (
+      <ul className="absolute left-0 top-0 h-screen w-3/4 py-12 px-6 lg:hidden flex flex-col bg-slate-200 gap-6">
+        <Logo />
+        {navLinks.map((link) => (
+          <li key={link.text} onClick={handleClick}>
+            <Link className="flex items-center gap-2" href={link.href}>
+              {link.icon}
+              {link.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderMenuLinks = () => {
+    return (
+      <ul className="hidden lg:flex gap-6">
+        {navLinks.map((link) => (
+          <li key={link.text}>
+            <Link className="flex items-center gap-2" href={link.href}>
+              {link.icon}
+              {link.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <nav className="relative">
       {isMenuOpen && (
@@ -51,33 +93,8 @@ const Navbar = (props: Props) => {
       )}
       <div className="container flex items-center justify-between">
         <Logo />
-
-        {/* Mobile Sidebar */}
-        {isMenuOpen && (
-          <ul className="absolute left-0 top-0 h-screen w-3/4 py-12 px-6 lg:hidden flex flex-col bg-slate-200 gap-6">
-            <Logo />
-            {navLinks.map((link) => (
-              <li key={link.text} onClick={handleClick}>
-                <Link className="flex items-center gap-2" href={link.href}>
-                  {link.icon}
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-
-        <ul className="hidden lg:flex gap-6">
-          {navLinks.map((link) => (
-            <li key={link.text}>
-              <Link className="flex items-center gap-2" href={link.href}>
-                {link.icon}
-                {link.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
+        {renderMobileSidebar()}
+        {renderMenuLinks()}
         <div className="flex gap-3 items-center">
           <ThemeSwitcher />
           {isMenuOpen ? (
